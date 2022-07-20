75ea016facda19fa2fddcdde51d3f535a5477a29
albertomancino <alberto.mancino94@gmail.com>
2022-07-20 09:36:30 +0000

    fix leaf error

--- parlai/agents/transformer_rec/modules.py
+++ parlai/agents/transformer_rec/modules.py
@@ -185,8 +185,8 @@ def create_position_codes(n_pos, dim, out):
         for pos in range(n_pos)
     ])
 
-    out[:, 0::2] = torch.FloatTensor(np.sin(position_enc)).type_as(out)
-    out[:, 1::2] = torch.FloatTensor(np.cos(position_enc)).type_as(out)
+    out[:, 0::2].data = torch.FloatTensor(np.sin(position_enc)).type_as(out)
+    out[:, 1::2].data = torch.FloatTensor(np.cos(position_enc)).type_as(out)
     out.detach_()
     out.requires_grad = False
 
